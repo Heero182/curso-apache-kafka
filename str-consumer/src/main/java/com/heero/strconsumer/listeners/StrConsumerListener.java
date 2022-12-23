422a131ce0c4c72c26c4f8af0b763191a132a221
Heider Bernardes <heider.silveira@tty2000.com.br>
2022-12-23 17:45:23 -0300

    record interceptor

--- str-consumer/src/main/java/com/heero/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/heero/strconsumer/listeners/StrConsumerListener.java
@@ -1,16 +1,20 @@
 package com.heero.strconsumer.listeners;
 
 import com.heero.strconsumer.custom.StrConsumerSustomListener;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
+    @SneakyThrows
     @StrConsumerSustomListener(groupId = "group-1")
     public void create(String message) {
        log.info("CREATE ::: Receive message {}", message);
+       throw new IllegalArgumentException("Exception ...");
     }
 
     @StrConsumerSustomListener(groupId = "group-1")
@@ -19,7 +23,7 @@ public class StrConsumerListener {
     }
 
 
-    @StrConsumerSustomListener(groupId = "group-2")
+    @KafkaListener(groupId = "group-2", containerFactory = "validMessageContainerFactory", topics = "str-topic")
     public void history(String message) {
         log.info("HISTORY ::: Receive message {}", message);
     }
