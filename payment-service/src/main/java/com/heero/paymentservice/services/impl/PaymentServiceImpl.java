1b1e5a802ffa8a411983675b23246b5d0dc6250d
Heider Bernardes <heider.silveira@tty2000.com.br>
2022-12-26 13:39:36 -0300

    add producer config

--- payment-service/src/main/java/com/heero/paymentservice/services/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/heero/paymentservice/services/impl/PaymentServiceImpl.java
@@ -2,14 +2,30 @@ package com.heero.paymentservice.services.impl;
 
 import com.heero.paymentservice.model.Payment;
 import com.heero.paymentservice.services.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-       log.info("PaymentServiceImpl Recebi o pagamento {}", payment);
+       log.info("Recebi o pagamento {}", payment);
+       Thread.sleep(1000);
+
+       log.info("Sending payment...");
+
+        kafkaTemplate.send("payment-topic", payment);
+
     }
 }
